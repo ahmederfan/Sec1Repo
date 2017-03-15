532d6cf8f77baba2b5c7374e8dd944b1b0c6da4c
ahmederfan <ahmed.erfan54@gmail.com>
2017-03-15 13:56:44 +0200

    Update Sec1Repo.java

--- src/sec1repo/Sec1Repo.java
+++ src/sec1repo/Sec1Repo.java
@@ -16,7 +16,7 @@ public class Sec1Repo {
      */
     public static void main(String[] args) { 
         // TODO code application logic here
-        System.out.println("Goodbye");
+       
     }
     
 }
