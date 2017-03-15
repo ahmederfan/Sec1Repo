ea3d7ddc827c5f76978502afe7a4b5ae81743276
Ahmed Helal <ahmedhassan96@windowslive.com>
2017-03-15 13:10:28 +0200

    Revert "Hello"
    This reverts commit f0e1ec4afc0e98b6399072b68ce716e499e8c792.

--- src/sec1repo/Sec1Repo.java
+++ src/sec1repo/Sec1Repo.java
@@ -17,7 +17,6 @@ public class Sec1Repo {
     public static void main(String[] args) { 
         System.out.println("hello");
         // TODO code application logic here
-        System.out.println("Hello Section 1");
     }
     
 }
