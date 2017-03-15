f98f82adec3c983853963cf5888279ff21c239f5
Hadeel Eladawy <hadeel305@gmail.com>
2017-03-15 13:36:14 +0200

    Deleting main

--- src/sec1repo/Sec1Repo.java
+++ src/sec1repo/Sec1Repo.java
@@ -15,10 +15,7 @@ public class Sec1Repo {
      * @param args the command line arguments
      */
     public static void main(String[] args) { 
-        System.out.println("hello");
-        int x;
-        // TODO code application logic here
-        System.out.println("Hello Section 1");
+        
     }
     
 }
